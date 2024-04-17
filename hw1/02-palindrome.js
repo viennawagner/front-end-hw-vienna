7e7bdfa6e416025bd12e37b01b2722a753b84914
vwagner <35669181+Dylanrwagner@users.noreply.github.com>
2024-04-17 12:10:48 -0700

    spell palindrome correctly

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,5 +1,5 @@
-//returns true if the given number is a palendrome, false otherwise
-const isPalendrome = (num) => {
+//returns true if the given number is a palindrome, false otherwise
+const isPalindrome = (num) => {
   const original = num.toString();
 
   //reverse string by splitting it into an array and reversing that, then rejoining
@@ -9,7 +9,7 @@ const isPalendrome = (num) => {
   return reversed === original;
 };
 
-//Display to user whether input was a palendrome or not
+//Display to user whether input was a palindrome or not
 // displays invalid input if number is negative
 const handleInput = () => {
   const input = document.getElementById("numberInput").value;
@@ -20,8 +20,8 @@ const handleInput = () => {
   if (input === NaN || input === "" || input < 0) {
     responseText = "Please enter a positive number";
     outputElement.className = "text-danger";
-  } else if (isPalendrome(input)) {
-    responseText = "Yes. This is a palendrome!";
+  } else if (isPalindrome(input)) {
+    responseText = "Yes. This is a palindrome!";
     outputElement.className = "text-success";
   } else {
     responseText = "No. Try again.";
