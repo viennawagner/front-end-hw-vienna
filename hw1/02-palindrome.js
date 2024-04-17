187c8b6d753858dbb23459179ec53c78220a3b08
vwagner <35669181+Dylanrwagner@users.noreply.github.com>
2024-04-17 11:59:26 -0700

    format code

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,39 +1,36 @@
-
 //returns true if the given number is a palendrome, false otherwise
 const isPalendrome = (num) => {
-    console.log(num);
-    const original = num.toString(); 
+  console.log(num);
+  const original = num.toString();
 
-    //reverse string by splitting it into an array and reversing that, then rejoining
-    const reversed = original.split('').reverse().join('');
+  //reverse string by splitting it into an array and reversing that, then rejoining
+  const reversed = original.split("").reverse().join("");
 
-    return reversed === original;
-}
+  return reversed === original;
+};
 
 //Display to user whether input was a palendrome or not
 // displays invalid input if number is negative
 const handleInput = () => {
-    const input = document.getElementById("numberInput").value;
-    let outputElement = document.getElementById("response");
-    let responseText;
+  const input = document.getElementById("numberInput").value;
+  let outputElement = document.getElementById("response");
+  let responseText;
 
-    //Determine response to display, error on negative numbers and NaN/empty input
-    if(input === NaN || input === '' || input < 0) {
-        responseText = "Please enter a positive number";
-        outputElement.className = "text-danger";
-    }
-    else if(isPalendrome(input)) {
-        responseText = "Yes. This is a palendrome!";
-        outputElement.className = "text-success";
-    }
-    else {
-        responseText = "No. Try again.";
-        outputElement.className = "text-danger";
-    }
+  //Determine response to display, error on negative numbers and NaN/empty input
+  if (input === NaN || input === "" || input < 0) {
+    responseText = "Please enter a positive number";
+    outputElement.className = "text-danger";
+  } else if (isPalendrome(input)) {
+    responseText = "Yes. This is a palendrome!";
+    outputElement.className = "text-success";
+  } else {
+    responseText = "No. Try again.";
+    outputElement.className = "text-danger";
+  }
 
-    outputElement.textContent = responseText;
-}
+  outputElement.textContent = responseText;
+};
 
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", handleInput);
