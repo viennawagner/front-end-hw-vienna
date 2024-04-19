0eb623a0c24ac7bf3ac67ed28a9f166c536bdaa4
vwagner <35669181+Dylanrwagner@users.noreply.github.com>
2024-04-19 11:10:22 -0700

    Fix code styling and update comments

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,23 +1,23 @@
-//returns true if the given number is a palindrome, false otherwise
+//Take a number as input, return true if it's a palendrome, false otherwise
 const isPalindrome = (num) => {
   const original = num.toString();
 
-  //reverse string by splitting it into an array and reversing that, then rejoining
-  //credit: https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+  //Since strings can't be reversed natively, split it into an array and reverse that
+  //Credit: https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
   const reversed = original.split("").reverse().join("");
 
   return reversed === original;
 };
 
 //Display to user whether input was a palindrome or not
-// displays invalid input if number is negative
+// display invalid input if number is negative
 const handleInput = () => {
-  const input = document.getElementById("numberInput").value;
+  const input = parseFloat(document.getElementById("numberInput").value);
   let outputElement = document.getElementById("response");
   let responseText;
 
   //Determine response to display, error on negative numbers and NaN/empty input
-  if (input === NaN || input === "" || input < 0) {
+  if (isNaN(input) || input === "" || input < 0) {
     responseText = "Please enter a positive number";
     outputElement.className = "text-danger";
   } else if (isPalindrome(input)) {
@@ -31,6 +31,4 @@ const handleInput = () => {
   outputElement.textContent = responseText;
 };
 
-const elem = document.querySelector("input");
-
-elem.addEventListener("input", handleInput);
+document.querySelector("#numberInput").addEventListener("input", handleInput);
