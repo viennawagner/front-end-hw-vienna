d4fd6497680efdc53ec78308c717a2a0437b328d
vwagner <35669181+Dylanrwagner@users.noreply.github.com>
2024-04-22 19:24:23 -0700

    complete exercise 02
    stylize 02-search
    
    Implement function to make cards for search exercise
    
    implement name search function
    
    Implement base logic for search exercise
    
    Fix search triggering on any click
    
    Make search function no longer case sensitive
    
    Allow multiple results on one line
    
    improve width of main content
    
    center heading text and search bar
    
    add margins to cards
    
    clear search history on new search
    
    update alignment of results
    
    Add error on character not found

--- hw2/02-search.js
+++ hw2/02-search.js
@@ -1,3 +1,51 @@
-// Add your code here
+//Find all characters whose name includes the given string
+// return a list of the names and birth years of the found characters
+const findCharacters = (to_match) => {
+  //String.includes is case sensitive, so we need to convert everything to lowercase
+  const to_match_lowercase = to_match.toLowerCase();
+  return characters.filter((c) =>
+    c.name.toLowerCase().includes(to_match_lowercase)
+  );
+};
 
-userInput.addEventListener('click', handleClick);
+//Create a card with the name and birth year of the given card
+// return an HTML element of the card created
+const createCard = (character) => {
+  let card = document.createElement("div");
+  let name_element = document.createElement("p");
+  let birth_year_element = document.createElement("p");
+
+  name_element.textContent = character.name;
+  birth_year_element.textContent = `Birth year: ${character.birth_year}`;
+  card.appendChild(name_element);
+  card.appendChild(birth_year_element);
+
+  name_element.setAttribute("class", "fw-bold text-center text-wrap");
+  birth_year_element.setAttribute("class", "text-center");
+  card.setAttribute("class", "card col-8 col-md-4 col-lg-3 m-1");
+
+  return card;
+};
+
+//Remove all child elements from the given element
+const clearChildren = (elem) => {
+  elem.replaceChildren();
+};
+
+//Find the characters picked by the user and add cards with their info
+// to the DOM
+const handleClick = () => {
+  const input = document.getElementById("userInput").value;
+  const out_element = document.getElementById("card-placements");
+  clearChildren(out_element);
+  for (const character of findCharacters(input)) {
+    out_element.appendChild(createCard(character));
+  }
+
+  //If there's no match, no children are added so firstChild is null which evaluates to false
+  if (!out_element.firstChild) {
+    out_element.textContent = "No characters matched your search";
+  }
+};
+
+document.getElementById("searchBtn").addEventListener("click", handleClick);
