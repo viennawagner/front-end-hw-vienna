e4474efac8835f6c88e4cf2de0f0d2c558bd74d7
vwagner <35669181+Dylanrwagner@users.noreply.github.com>
2024-04-24 12:55:53 -0700

    Get exercise 5 started
    run formatter
    
    Stylize page
    
    Rename some elements, fix broken label
    
    Add HTML for the modal
    
    Implement baseline HTML for form exercise

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,13 @@
-// Add your code here
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById('userForm');
+    const modal_element = document.getElementById('responseModal');
+
+    const handleSubmit = (event) => {
+        //Prevent page reload on submit
+        event.preventDefault();
+
+        console.log('Event hit');
+    }
+
+    form.addEventListener('submit', handleSubmit);
+})
