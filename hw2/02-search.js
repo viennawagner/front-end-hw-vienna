4b8b09d478d632b65f609d7e403ab34c7e41c31d
vwagner <35669181+Dylanrwagner@users.noreply.github.com>
2024-05-01 13:26:46 -0700

    Add highlight functionality to search exercise

--- hw2/02-search.js
+++ hw2/02-search.js
@@ -1,11 +1,13 @@
 /* Find the characters picked by the user and add cards with their info
    to the DOM */
 const handleClick = () => {
-  const input = document.getElementById("userInput").value;
+  const input = document.getElementById("userInput").value.toLowerCase();
   const out_element = document.getElementById("card-placements");
   clearChildren(out_element);
   for (const character of findCharacters(input)) {
-    out_element.appendChild(createCard(character));
+    card = createCard(character);
+    highlightSearchTerm(input, card);
+    out_element.appendChild(card);
   }
 
   //If there's no match, no children are added so firstChild is null which evaluates to false
@@ -24,7 +26,7 @@ const clearChildren = (elem) => {
    return a list of the names and birth years of the found characters */
 const findCharacters = (to_match) => {
   //String.includes is case sensitive, so we need to convert everything to lowercase
-  const to_match_lowercase = to_match.toLowerCase();
+  const to_match_lowercase = to_match;
   return characters.filter((c) =>
     c.name.toLowerCase().includes(to_match_lowercase)
   );
@@ -42,12 +44,20 @@ const createCard = (character) => {
   card.appendChild(name_element);
   card.appendChild(birth_year_element);
 
-  name_element.setAttribute("class", "fw-bold text-center text-wrap");
+  name_element.setAttribute("class", "fw-bold text-center text-wrap name");
   birth_year_element.setAttribute("class", "text-center");
   card.setAttribute("class", "card col-8 col-md-4 col-lg-3 m-1");
 
   return card;
 };
 
+// Highlight search term in given card's name
+const highlightSearchTerm = (term, card)  => {
+  console.log(card.firstChild);
+  let name_element = card.querySelector(".name");
+  const name = name_element.textContent;
+  const regex_term = new RegExp(`(${term})`, "ig");
+  name_element.innerHTML = name.replace(regex_term, "<mark class='p-0' style='background-color: yellow;'>$1</mark>");
+}
 
 document.getElementById("searchBtn").addEventListener("click", handleClick);
