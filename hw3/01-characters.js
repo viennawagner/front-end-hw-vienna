ed301bd5c0502638513eb21598ec3bb0266365e7
vwagner <35669181+Dylanrwagner@users.noreply.github.com>
2024-05-21 12:20:46 -0700

    Import base project

--- hw3/01-characters.js
+++
@@ -1,4 +0,0 @@
-// url for the Thrones API
-const url = 'https://thronesapi.com/api/v2/Characters';
-
-fetch(url);
---
+++ hw3/src/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function Navbar() {
+  return (
+    <div class="navbar">
+      <a href="index.js" class="navbar-item"> Home </a>
+    </div>
+  )
+}
